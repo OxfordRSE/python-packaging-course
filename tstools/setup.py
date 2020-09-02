69ea5e199a5badb31a277beab3a15649c97db6f8
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-09-02 10:12:40 +0100

    Add setup.py for setuptools

---
+++ tstools/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tstools',
+      version='0.1',
+      description='A package to analyse timeseries',
+      url='',
+      author='Spam Eggs',
+      author_email='spameggs@example.com',
+      packages=['tstools'],
+      license='GPLv3')
