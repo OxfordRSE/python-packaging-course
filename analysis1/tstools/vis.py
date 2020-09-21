4b27fb2753c17b6f6a9c3a9a1954e98eda7c9203
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-09-21 08:55:21 +0100

    Get rid of ex1 dir and add 3 incomplete modules in analysis1/tstools/ directory

--- analysis1/tstools/vis.py
+++ analysis1/tstools/vis.py
@@ -1,25 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.special import erf
-import matplotlib.pyplot as plt
-
-
-def get_mean_and_var(timeseries):
-    """
-    Compute and return the average (empirical mean) and empirical variance
-    over TIMESERIES.
-
-    Parameters
-    ----------
-    timeseries: numpy.array
-        The timeseries
-
-    Returns
-    -------
-    mean, var: float
-        A tuple which first element is the mean and second the variance.
-    """
-
-    return np.mean(timeseries[:, 1]), "######"
 
 
 def plot_trajectory_subset(timeseries, tmin, tmax):
