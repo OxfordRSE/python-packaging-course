30e47a87a72f1d8292958563ae6d30b84aa123a7
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-11-10 11:23:11 +0000

    Plot hotwire signal in analysis2

--- .cheat/analysis2/analysis2.py
+++ .cheat/analysis2/analysis2.py
@@ -1,10 +1,17 @@
 # analysis2/analysis2.py
 import sys
+import matplotlib.pyplot as plt
 import numpy as np
 
 sys.path.append("../analysis1/")
 
 import tstools
 
-timeseries = np.genfromtxt("./data/data_analysis2.csv", delimiter=",")
-fig, ax = tstools.plot_trajectory_subset(timeseries, 0, 50)
+timeseries = np.genfromtxt("./data/hotwire.csv", delimiter=",")
+fig, ax = tstools.plot_trajectory_subset(timeseries, 0, 0.25)
+
+ax.set_ylabel("$u_x(t)$ (m/s)", fontsize=16)
+ax.set_xlabel("$t$ (s)", fontsize=16)
+
+ax.plot()
+plt.show()
