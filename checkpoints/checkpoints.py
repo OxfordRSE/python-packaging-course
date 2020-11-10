431ce74bcd5e7735adf46d6272b8b65eb22399f8
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2020-11-10 12:23:21 +0000

    Add checkpoint4 to checkpoints

--- checkpoints/checkpoints.py
+++ checkpoints/checkpoints.py
@@ -3,11 +3,13 @@ import os
 import shutil
 
 
-def copy_files(files_list: list):
+def copy_files(files_list: list, new_names=[]):
     """Copy files in FILES_LIST from .cheat/ to course/"""
-    for filename in files_list:
+    if not new_names:
+        new_names = files_list
+    for filename, new_filename in zip(files_list, new_names):
         cheat_file = os.path.join(".cheat", filename)
-        file_to_replace = os.path.join("course", filename)
+        file_to_replace = os.path.join("course", new_filename)
         try:
             shutil.copy2(cheat_file, file_to_replace)
         except FileNotFoundError as error:
@@ -25,6 +27,7 @@ def checkpoint1():
         "analysis1/tstools/vis.py",
         "analysis1/tstools/moments.py",
         "analysis1/tstools/extremes.py",
+        "analysis1/analysis1.py",
     ]
     copy_files(files)
 
@@ -35,7 +38,7 @@ def checkpoint2():
 
 
 def checkpoint3():
-    if not os.is_dir(".cheat"):
+    if not os.path.isdir(".cheat"):
         msg = (
             "could not setup course files."
             " Are you in the course top-level directory?"
@@ -45,8 +48,8 @@ def checkpoint3():
 
     # Not using shutil.copytree because option dir_exists_ok only
     # available since python 3.8
-    for directory in ["tstools-dist", os.path("tstools-dist", "tstools")]:
-        if not os.is_dir(directory):
+    for directory in ["tstools-dist", os.path.join("tstools-dist", "tstools")]:
+        if not os.path.isdir(directory):
             os.mkdir(directory)
 
     files = [
@@ -56,6 +59,18 @@ def checkpoint3():
         "tstools-dist/tstools/extremes.py",
     ]
     copy_files(files)
+
+
+def checkpoint4():
+    files = [
+        "analysis2/analysis2_centered.py",
+        "tstools-dist/tstools/centered/vis.py",
+    ]
+    new_files = [
+        "analysis2/analysis2_centered.py",
+        "tstools-dist/tstools/vis.py",
+    ]
+    copy_files(files, new_files)
 
 
 def main(arguments=None):
@@ -75,6 +90,7 @@ def main(arguments=None):
         1: checkpoint1,
         2: checkpoint2,
         3: checkpoint3,
+        4: checkpoint4,
     }
 
     map_number_to_function[args.checkpoint_number]()
