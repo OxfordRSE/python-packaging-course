9f9a7b64c968a744e050555e64cd0aab48010512
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-03-23 14:42:24 +0000

    Remove calculation of extremes

--- .cheat/analysis1/analysis1.py
+++ .cheat/analysis1/analysis1.py
@@ -4,18 +4,13 @@ import matplotlib.pyplot as plt
 
 import tstools.moments
 import tstools.vis
-import tstools.extremes
 
 timeseries = np.genfromtxt("./data/brownian.csv", delimiter=",")
 
 mean, var = tstools.moments.get_mean_and_var(timeseries)
 
-fig1, ax1 = tstools.vis.plot_histogram(timeseries, nbins=100)
+fig, ax = tstools.vis.plot_histogram(timeseries, nbins=100)
 
-threshold = 3 * np.sqrt(var)
-fig2, ax2 = tstools.extremes.show_extremes(timeseries, threshold)
-
-ax1.plot()
-ax2.plot()
+ax.plot()
 
 plt.show()
--- .cheat/analysis1/analysis1_w_init.py
+++ .cheat/analysis1/analysis1_w_init.py
@@ -1,18 +1,15 @@
 # analysis1/analysis1.py
 import numpy as np
 import matplotlib.pyplot as plt
+
 import tstools
 
 timeseries = np.genfromtxt("./data/brownian.csv", delimiter=",")
 
 mean, var = tstools.get_mean_and_var(timeseries)
 
-fig1, ax1 = tstools.plot_histogram(timeseries, nbins=100)
+fig, ax = tstools.plot_histogram(timeseries, nbins=100)
 
-threshold = 3 * np.sqrt(var)
-fig2, ax2 = tstools.show_extremes(timeseries, threshold)
-
-ax1.plot()
-ax2.plot()
+ax.plot()
 
 plt.show()
