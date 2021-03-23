df949e104ab3b07069ebc0f3c12ca03aff1fe582
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-03-23 14:55:51 +0000

    Remove plot of gaussian histogram

--- course/initial_scripts/analysis.py
+++ course/initial_scripts/analysis.py
@@ -26,21 +26,4 @@ bin_centers = 0.5 * (bin_edges[1:] + bin_edges[0:-1])
 fig2, ax2 = plt.subplots()
 ax2.plot(bin_centers, hist, "*")
 
-
-# Now plot the "true" histogram (gaussian PDF)
-def get_theoritical_histogram(fluct_min, fluct_max, std, mean, nbins=100):
-    bin_edges = np.linspace(fluct_min, fluct_max, nbins + 1)
-    nb_samples = len(timeseries[:, 1])
-    rescaled_bin_edges = (bin_edges - mean) / (std * np.sqrt(2))
-    hist = (
-        0.5 * nb_samples * (erf(rescaled_bin_edges[1:]) - erf(rescaled_bin_edges[:-1]))
-    )
-    return hist, bin_edges
-
-
-hist, bin_edges = get_theoritical_histogram(
-    np.amin(bin_edges), np.amax(bin_edges), std, mean
-)
-ax2.plot(bin_centers, hist)
-
 plt.show()
