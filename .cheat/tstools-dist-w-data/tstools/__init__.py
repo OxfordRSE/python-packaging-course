eee6c1c590733c9dc8420e17e00afc077c682bd6
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-03-23 14:52:48 +0000

    Remove calculation of extremes

--- .cheat/tstools-dist-w-data/tstools/__init__.py
+++ .cheat/tstools-dist-w-data/tstools/__init__.py
@@ -1,3 +1,2 @@
 from .moments import *
 from .vis import *
-from .extremes import *
--- .cheat/tstools-dist/__init__.py
+++ .cheat/tstools-dist/__init__.py
@@ -1,3 +1,2 @@
 from .moments import get_mean_and_var
 from .vis import plot_trajectory_subset, plot_histogram
-from .extremes import show_extremes
